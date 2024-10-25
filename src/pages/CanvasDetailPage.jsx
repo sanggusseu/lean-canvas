fe4ce787d5e720b70a7d621488d9bc39fbb7f879
sanggusseu <sanggusseu@gmail.com>
2024-10-25 09:36:22 +0900

    레이아웃 UI

--- src/pages/CanvasDetailPage.jsx
+++ src/pages/CanvasDetailPage.jsx
@@ -1,9 +1,11 @@
 import CanvasTitle from '../components/canvas/CanvasTitle';
+import LeanCanvas from '../components/canvas/LeanCanvas';
 
 export default function CanvasDetailPage() {
   return (
     <div>
       <CanvasTitle />
+      <LeanCanvas />
     </div>
   );
 }
