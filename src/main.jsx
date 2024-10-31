b24ed4033c11344c9533317caaa4d995174d6554
sanggusseu <sanggusseu@gmail.com>
2024-10-31 11:52:52 +0900

    about 페이지에서 react-query 실습

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import AboutPage from './pages/AboutPage.jsx';
 import ContactPage from './pages/ContactPage.jsx';
 import ErrorPage from './pages/ErrorPage.jsx';
 import CanvasDetailPage from './pages/CanvasDetailPage.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter([
   {
@@ -35,8 +36,12 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,3 +1,37 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+
 export default function AboutPage() {
-  return <div>About</div>;
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['canvases'],
+    queryFn: () =>
+      axios.get('http://localhost:8000/canvases/').then(res => res.data),
+    initialData: [],
+  });
+  const queryClient = useQueryClient();
+
+  const { mutate: createNewCanvas, isLoading: isLoadingCreate } = useMutation({
+    mutationFn: newCanvas => {
+      axios.post('http://localhost:8000/canvases/', newCanvas);
+    },
+    onSuccess: queryClient.invalidateQueries(['canvases']),
+  });
+  const handleCreate = () => {
+    createNewCanvas({ title: 'new canvas' });
+  };
+  return (
+    <div>
+      <h2 className="text-2xl">useQuery</h2>
+      {isLoading && <p>loading...</p>}
+      {error && <p className="text-red-700">{error.message}</p>}
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleCreate}>
+        등록하기
+      </button>
+    </div>
+  );
 }
